5b5af97e9f4b86143e05d25c0b80eb5906b63f38
Bartosz Kotrys <bartosz.kotrys@gmail.com>
2018-12-04 16:02:15 +0100

    feat: migrate to yarn

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import express from "express";
 import { Request, Response } from "express";
 
 const app = express();
-const port = 8080; // default port to listen
-
-// define a route handler for the default home page
-app.get( "/", ( req: Request, res: Response ) => {
+const port = 8080; 
+        
+app.get("/", (req: Request, res: Response ) => {
     res.send( "Hello world!" );
-} );
+}  );
 
-// start the Express server
 app.listen( port, () => {
     console.warn( `server started at http://localhost:${ port }` );
 } );
