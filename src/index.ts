9de00bb1a6a5f6a8753e02a219a1ecffc3fc47f6
Bartosz Kotrys <bartosz.kotrys@gmail.com>
2018-12-07 15:31:02 +0100

    feat(config): improve basic setup

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import { Request, Response } from 'express';
 
 const app = express();
-const port = 8080; 
+const port = 8080;
 
 app.get('/', (req: Request, res: Response ) => {
-    res.send( 'Hello world!' );
-}  );
+  res.send( 'Hello world!' );
+});
 
 app.listen( port, () => {
-    console.warn(`server started at http://localhost:${ port }`);
-} );
+  console.warn(`server started at http://localhost:${ port }`);
+});
